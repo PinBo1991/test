2f3bdd9575e495de7ef2ffa8f186912f21e7ee8c
ChaiXY <ChaiXY_0403@163.com>
2017-12-28 15:06:18 +0800

    POI操作Excel,String的一些操作

---
+++ test/src/main/java/com/grt/common/utils/StringIndex.java
@@ -0,0 +1,31 @@
+package com.grt.common.utils;
+
+public class StringIndex {
+	
+	public static void main(String[] args) {
+//		String str = "asdfasdf";
+//		int indexOf = str.indexOf("a");
+//		System.out.println(indexOf);
+//		String[] split = str.split("s");
+//		for(int i=0;i<split.length;i++){
+//			System.out.print(split[i]+" ");
+//		}
+		
+//        String str = "div abc div bb div c div dde",
+//               des = "div";
+//         
+//        System.out.println(str.indexOf(2, 4));
+//        
+//        int cnt = 0;
+//        int offset = 0;
+//        while((offset = str.indexOf(des, offset)) != -1){
+//        	System.out.println(offset);
+//            offset = offset + des.length();
+//            cnt++;
+//        }
+//        System.out.println(cnt);
+//		
+//        System.out.println(str.length());
+//        System.out.println(str.substring(28)+"-");
+	}
+}
