df5a6e8d3321fa8229a6f8a8babf8a400f11569a
ChaiXY <ChaiXY_0403@163.com>
2018-01-13 21:38:21 +0800

    if else if使用

---
+++ test/src/main/java/com/grt/test/IFElseIfTest.java
@@ -0,0 +1,22 @@
+package com.grt.test;
+
+public class IFElseIfTest {
+	public static void test01(){
+//		if (1 == 1) {
+//			System.out.println("第一个分支执行");
+//		} else if (1 == 1) {
+//			System.out.println("第二个分支执行");
+//		}
+		
+		if(1 == 1){
+			System.out.println("第一个分支执行");
+		}
+		if(1 == 1){
+			System.out.println("第二个分支执行");
+		}
+	}
+	
+	public static void main(String[] args) {
+		test01();
+	}
+}
